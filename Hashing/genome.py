ef0c4f2e74c31509fdb2962d092fa2cfc4c0f378
Angus Gruen <angusgruen@gmail.com>
2017-02-09 22:13:46 +1100

    Re-wrote the genome generator as functions. Created a generator to make less random data to better test countmin.

--- Hashing/genome.py
+++ Hashing/genome.py
@@ -1,7 +1,41 @@
 import random
-# This simple generates a genome (Organism).
+import numpy as np
 
-f = open('genome_short.txt', 'w')
+def genome(x):
 
-for _ in range(0, 10**4):
-	f.write(random.choice('ATCG'))
+	# This function generates a genome (Organism) for length x.
+	# Essentially writes a file consisting of a word of length x over the alphabet {A, T, C, G}
+
+	f = open('genome.txt', 'w')
+
+	for _ in range(x):
+		f.write(random.choice('ATCG'))
+
+def markov_genome(x):
+
+	# This function generates a genome (Organism) for length x.
+	# Essentially writes a file consisting of a word of length x over the alphabet {A, T, C, G}
+	# The difference from the last function is that the previous letter influences the following.
+
+	f = open('markov_genome.txt', 'w')
+
+	array = np.array(['A', 'T', 'C', 'G'])
+
+	letter = random.choice('ATCG')
+	f.write(letter)
+
+	for _ in range(x - 1):
+		if letter == 'A':
+			letter = np.random.choice(array, p=[0.4, 0.3, 0.2, 0.1])
+			f.write(letter)
+		if letter == 'T':
+			letter = np.random.choice(array, p=[0.1, 0.4, 0.3, 0.2])
+			f.write(letter)
+		if letter == 'C':
+			letter = np.random.choice(array, p=[0.2, 0.1, 0.4, 0.3])
+			f.write(letter)
+		if letter == 'G':
+			letter = np.random.choice(array, p=[0.3, 0.2, 0.1, 0.4])
+			f.write(letter)
+
+markov_genome(100000)
